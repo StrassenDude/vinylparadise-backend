713f705c2bc81a969904ff21f5be9dd14735f78a
taylor <taylor@akaryon.com>
2022-03-17 16:05:26 +0100

    Added Fileupload //No testing

--- src/main/java/com/example/vinylparadise/VinylparadiseApplication.java
+++ src/main/java/com/example/vinylparadise/VinylparadiseApplication.java
@@ -1,9 +1,15 @@
 package com.example.vinylparadise;
 
+import com.example.vinylparadise.storage.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+//enable the ConfigurationProperties
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class VinylparadiseApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/vinylparadise/controller/UserController.java
+++ src/main/java/com/example/vinylparadise/controller/UserController.java
@@ -116,7 +116,6 @@ public class UserController {
 
         userRepository.delete(user);
 
-
     }
 
 }
